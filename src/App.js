7fef3d3d2c03b64514fec4a2c821ac64abc81023
Hendrik Human <jh.humsn@gmail.com>
2022-02-15 15:49:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import one  from './1.jpg';
+import two  from './2.jpg';
+import three  from './3.jpg';
+import four  from './4.jpg';
+import five  from './5.jpg';
+import six  from './6.jpg';
+import seven  from './7.jpg';
+import eight  from './8.jpg';
+import nine  from './9.jpg';
+import ten  from './10.jpg';
+import eleven  from './11.jpg';
+import twelve  from './12.jpg';
+import thirteen  from './13.jpg';
+import fourteen  from './14.jpg';
+import fifteen  from './15.jpg';
+import sixteen  from './16.jpg';
+import seventeen  from './17.jpg';
+import eighteen  from './18.jpg';
+import nineteen  from './19.jpg';
+import twenty  from './20.jpg';
+import twenty1  from './21.jpg';
+import twenty2  from './22.jpg';
+import twenty3  from './23.jpg';
+import twenty4  from './24.jpg';
+import twenty5  from './25.jpg';
+import twenty6  from './26.jpg';
+import twenty7  from './27.jpg';
+import twenty8  from './28.jpg';
+import twenty9  from './29.jpg';
+import thirty from './30.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <img src={one} alt="first pic"/>
+    <img src={two} alt="first pic"/>
+    <img src={three} alt="first pic"/>
+    <img src={four} alt="first pic"/>
+    <img src={five} alt="first pic"/>
+    <img src={six} alt="first pic"/>
+    <img src={seven} alt="first pic"/>
+    <img src={eight} alt="first pic"/>
+    <img src={nine} alt="first pic"/>
+    <img src={ten} alt="first pic"/>
+    <img src={eleven} alt="first pic"/>
+    <img src={twelve} alt="first pic"/>
+    <img src={thirteen} alt="first pic"/>
+    <img src={fourteen} alt="first pic"/>
+    <img src={fifteen} alt="first pic"/>
+    <img src={sixteen} alt="first pic"/>
+    <img src={seventeen} alt="first pic"/>
+    <img src={eighteen} alt="first pic"/>
+    <img src={nineteen} alt="first pic"/>
+    <img src={twenty} alt="first pic"/>
+    <img src={twenty1} alt="first pic"/>
+    <img src={twenty2} alt="first pic"/>
+    <img src={twenty3} alt="first pic"/>
+    <img src={twenty4} alt="first pic"/>
+    <img src={twenty5} alt="first pic"/>
+    <img src={twenty6} alt="first pic"/>
+    <img src={twenty7} alt="first pic"/>
+    <img src={twenty8} alt="first pic"/>
+    <img src={twenty9} alt="first pic"/>
+    <img src={thirty} alt="first pic"/>
   );
 }
 
